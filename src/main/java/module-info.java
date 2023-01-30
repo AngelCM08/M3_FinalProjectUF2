88b9b088f49231fbaa9a673dd5cc615e27f3cc68
Ángel <angelcastromerino@gmail.com>
2023-01-30 15:29:41 +0100

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.m3_finalprojectuf2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.m3_finalprojectuf2 to javafx.fxml;
+    exports com.example.m3_finalprojectuf2;
+}
